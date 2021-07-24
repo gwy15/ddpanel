68d8b30cc2fd1480101b01df7c9acf25c0549bf4
gwy15 <gwy15thu@gmail.com>
2021-07-24 19:42:09 +0800

    支持在直播间送给另外主播礼物

--- src/influx/messages/send_gift.rs
+++ src/influx/messages/send_gift.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Local};
 use std::fmt::{self, Display, Formatter};
 
 use influxdb_client::Point;
@@ -20,7 +21,19 @@ pub struct SendGift {
 
     #[serde(rename = "uname")]
     sender_name: String,
+
+    /// 现在可以在某个直播间送给另一个主播礼物
+    #[serde(default, rename = "send_master")]
+    gift_receiver: Option<GiftReceiver>,
 }
+/// 现在可以在某个直播间送给另一个主播礼物
+#[derive(Debug, Deserialize, Clone)]
+pub struct GiftReceiver {
+    room_id: u64,
+    uid: u64,
+    uname: String,
+}
+
 impl SendGift {
     pub fn price(&self) -> f64 {
         if self.coin_type == "gold" {
@@ -37,17 +50,35 @@ impl Display for SendGift {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         if self.coin_type == "gold" {
             f.write_fmt(format_args!(
-                "Gift {{ {} * {} = ￥{}}}",
+                "Gift {{ {} * {} = ￥{}",
                 self.gift_name,
                 self.num,
                 self.price()
-            ))
+            ))?;
+            if let Some(receiver) = &self.gift_receiver {
+                f.write_fmt(format_args!(" => {}", receiver.uname))?;
+            }
+            f.write_str(" }")
         } else {
             f.write_fmt(format_args!("Gift {{ {} * {} }}", self.gift_name, self.num))
         }
     }
 }
 impl super::ToPoint for SendGift {
+    fn into_point(self, room_info: &crate::influx::RoomInfo, t: DateTime<Local>) -> Point {
+        let receiver = self.gift_receiver.clone();
+        let pt = self.into_basic_point();
+        match receiver {
+            Some(receiver) => pt
+                .tag("room_id", receiver.room_id.to_string())
+                .tag("streamer", receiver.uname),
+            None => pt
+                .tag("room_id", room_info.id.to_string())
+                .tag("streamer", room_info.streamer.as_str()),
+        }
+        .timestamp(t.timestamp_millis())
+    }
+
     fn into_basic_point(self) -> Point {
         if !self.is_free() {
             let price = self.price();
